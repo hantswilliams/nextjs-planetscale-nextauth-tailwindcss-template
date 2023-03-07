78b7450507d9a4b150bf93bcb1f1234ce2ddbbbc
Hants Williams <hantsawilliams@gmail.com>
2023-03-07 15:00:21 -0500

    remove pnpm

--- pages/api/images/upload-image-s3.ts
+++ pages/api/images/upload-image-s3.ts
@@ -5,15 +5,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   try {
     // 1.
     const s3 = new aws.S3({
-      accessKeyId: process.env.AWS_ACCESS_KEY,
-      secretAccessKey: process.env.AWS_SECRET_KEY,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
       region: process.env.AWS_REGION,
     })
 
     // 2.
     aws.config.update({
-      accessKeyId: process.env.AWS_ACCESS_KEY,
-      secretAccessKey: process.env.AWS_SECRET_KEY,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
       region: process.env.AWS_REGION,
       signatureVersion: 'v4',
     })
