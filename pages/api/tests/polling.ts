8fe751499aea7fe064b9b2d7c749cb1443ec9dde
Hants Williams <hantsawilliams@gmail.com>
2023-03-14 06:54:14 -0500

    removed chunkced for testing, next should try g-zip

--- pages/api/tests/polling.ts
+++ pages/api/tests/polling.ts
@@ -231,7 +231,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
-  res.setHeader("Transfer-Encoding", "chunked");
+  // res.setHeader("Transfer-Encoding", "chunked");
   res.setHeader("Content-Encoding", "none");
 
 
