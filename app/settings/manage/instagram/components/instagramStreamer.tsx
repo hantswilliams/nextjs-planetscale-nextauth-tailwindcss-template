0dbee05b3fac2065376214bd5eb4ab63d99ae86b
Hants Williams <hantsawilliams@gmail.com>
2023-03-16 07:59:09 -0500

    testing for prod

--- app/settings/manage/instagram/components/instagramStreamer.tsx
+++ app/settings/manage/instagram/components/instagramStreamer.tsx
@@ -12,6 +12,8 @@ const InstagramStreamer: React.FC<InstagramStreamerProps> = ({ userIguserId, use
   const [progress, setProgress] = useState<number | null>(null);
   const [status, setStatus] = useState<string>('');
   const [streamStarted, setStreamStarted] = useState<boolean>(false);
+  const [chunksAccumulated, setChunksAccumulated] = useState('');
+
 
   const fetchData = useCallback(async () => {
     const response = await fetch('/api/retrieve/instagram/get-streaming/', {
@@ -28,23 +30,30 @@ const InstagramStreamer: React.FC<InstagramStreamerProps> = ({ userIguserId, use
 
     //@ts-expect-error
     reader.read().then(async function processText({ done, value }) {
-        if (done) {
-          console.log('Stream complete.');
-          return;
-        }
-      
-        const chunk = decoder.decode(value, { stream: true });
-        console.log('chunk received: ', chunk);
-      
-        // Accumulate received chunks
-        let accumulatedChunks = chunk;
-      
-        // Keep looping until no more valid JSON objects can be extracted
-        while (true) {
+      if (done) {
+        console.log('Stream complete.');
+        return;
+      }
+    
+      const chunk = decoder.decode(value, { stream: true });
+      console.log('chunk received: ', chunk);
+    
+      // Accumulate received chunks
+      setChunksAccumulated(chunksAccumulated + chunk);
+    
+      // Keep looping until no more valid JSON objects can be extracted
+      while (true) {
+        const openBracketIndex = chunksAccumulated.indexOf('{');
+        const closeBracketIndex = chunksAccumulated.indexOf('}', openBracketIndex);
+    
+        // If both opening and closing brackets are found, extract JSON string
+        if (openBracketIndex !== -1 && closeBracketIndex !== -1) {
+          const jsonString = chunksAccumulated.slice(openBracketIndex, closeBracketIndex + 1);
+    
           try {
-            // Attempt to parse a JSON object from the accumulatedChunks
-            const parsedChunk = JSON.parse(accumulatedChunks);
-      
+            // Attempt to parse a JSON object from the jsonString
+            const parsedChunk = JSON.parse(jsonString);
+    
             // If parsing is successful, process the parsed JSON object
             if (parsedChunk.starting) {
               setStatus('Starting...');
@@ -56,18 +65,24 @@ const InstagramStreamer: React.FC<InstagramStreamerProps> = ({ userIguserId, use
             } else if (parsedChunk.finished) {
               setStatus('Finished!');
             }
-      
-            // Clear accumulatedChunks since the JSON object was successfully processed
-            accumulatedChunks = '';
+    
+            // Remove the processed JSON string from chunksAccumulated
+            setChunksAccumulated(chunksAccumulated.slice(0, openBracketIndex) + chunksAccumulated.slice(closeBracketIndex + 1));
+    
           } catch (error) {
-            // If accumulatedChunks contains an incomplete JSON object, break the loop
+            // If jsonString contains an incomplete JSON object, break the loop
             // and wait for the next chunk
             break;
           }
+        } else {
+          // If no complete JSON string is found, break the loop and wait for the next chunk
+          break;
         }
-      
-        return reader.read().then(processText);
-      });      
+      }
+    
+      return reader.read().then(processText);
+    });
+          
   }, [userIguserId, userIguserToken, userCurrentuserId]);
 
   useEffect(() => {
